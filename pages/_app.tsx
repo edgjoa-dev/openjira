7da5e573067b57576aba668f1379eb80d460f2a8
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-14 12:25:45 -0600

    feature: Configuración de temas desde MaterialUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-//import { ThemeProvider } from '@mui/material'
+import { darkTheme, LightTheme } from 'themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-//    <ThemeProvider theme={}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Component {...pageProps} />
-//    </ThemeProvider>
+    </ThemeProvider>
   )
 }
---
+++ themes/light-theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles'
+
+export const LightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    }
+})
+
+
